d6513518310b2d3fdf0dcf6bad4a30d83db68952
EC2 Default User <ec2-user@ip-172-31-10-222.ap-northeast-1.compute.internal>
2023-10-07 06:11:13 +0000

    投稿した画像が見れるようになった、退会画面完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,14 +5,14 @@ class Post < ApplicationRecord
   has_many :greats, dependent: :destroy
 
 
-  has_one_attached :profile_image#画像用のカラム名
+  has_one_attached :profile_image #画像用のカラム名
 
   def get_profile_image(width, height)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
-  end
-  profile_image.variant(resize_to_limit: [width, height]).processed
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
